50f52542d47a6babb0c32944400da08ac214fdd6
behi_ <behi_@DESKTOP-HCME2RV>
2016-11-30 09:27:48 +0100

    added FileDoc and Visitor

--- Kompositum/src/Component.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class Component {
-
-	private ArrayList<File> files = new ArrayList<File>();
-	private ArrayList<Directory> directories = new ArrayList<Directory>();
-	
-	
-}
---
+++ Kompositum/src/Visitor.java
@@ -0,0 +1,7 @@
+
+public interface Visitor {
+	
+	public void visit(File file);
+	public void visit(Directory directory);
+
+}
